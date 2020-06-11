5f324eba06207744a5a31ce1b7350b892241d54f
nyankyo <66740694+nyankyo@users.noreply.github.com>
2020-06-10 20:30:29 -0400

    Added file via upload
    File created for lab 1.

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/*******************************
+*Lab 1
+********************************
+*Cash Frost
+*June 12, 2020 (06/12/20)
+CMSC255 002
+*******************************/
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("<Cash Frost>");
+		System.out.println("<Lab 1>");
+		System.out.println("<Basic code to test out required programs for the semester.>");
+		System.out.println("<June 12, 2020>");
+		System.out.println("<CMSC255 002>");
+		System.out.println();
+		}
+}
