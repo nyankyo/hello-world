f1fad782b434288f099832400452b16a969ecae3
nyankyo <66740694+nyankyo@users.noreply.github.com>
2020-08-25 10:20:22 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/*******************************
+*Lab 1
+********************************
+*Cash Frost
+*August 25, 2020 (08/25/20)
+CMSC255 002
+*******************************/
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("<Cash Frost>");
+		System.out.println("<Lab 1>");
+		System.out.println("<Basic code to test out required programs for the semester.>");
+		System.out.println("<August 25, 2020>");
+		System.out.println("<CMSC255 002>");
+		System.out.println();
+		}
+}
